93c122644e81006841d54c2e1ff2e7f97fa173af
michal.wasilewski <michal.wasilewski696@gmail.com>
2021-01-20 17:31:21 +0000

    controller & cleaning functions

--- src/App.js
+++ src/App.js
@@ -11,8 +11,20 @@ function App() {
   const [passwordError, setPasswordError] = useState('');
   const [hasAccount, setHasAccount] = useState(false);
 
+  // Cleaning inputs and errors
+  const clearInputs = () => {
+    setEmail('');
+    setPassword('');
+  };
+
+  const clearErrors = () => {
+    setEmailError('');
+    setPasswordError('');
+  }
+
   // Controllers
   const handleLogin = () => {
+    clearErrors();
     fire
     .auth()
     .signInWithEmailAndPassword(email, password)
@@ -27,9 +39,48 @@ function App() {
           setPasswordError(err.message);
           break;
       }
-    }) 
-  }
+    }); 
+  };
 
+  const handleSignUp = () => {
+    clearErrors();
+    fire
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(err => {
+      switch (err.code) {
+        case "auth/email-already-in-use":
+        case "auth/invalid-email":
+          setEmailError(err.message);
+          break;
+        case "auth/weak-password":
+          setPasswordError(err.message);
+          break;
+      }
+    }); 
+  };
+
+  const handleLogout = () => {
+    fire.auth().signOut();
+  };
+
+  const authListener = () => {
+      fire.auth().onAuthStateChanged((user) => {
+        if (user) {
+          clearInputs();
+          setUser(user);
+        } else {
+          setUser('');
+        }
+      });
+  };
+
+  useEffect(() => {
+    authListener();
+  }, []);
+
+  
+  // app
   return (
     <div className="App">
       
